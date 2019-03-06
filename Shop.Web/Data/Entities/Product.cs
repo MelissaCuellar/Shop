83649fa1186f38a8d68a4f1e0b4dd618ee5f76f9
MelissaCuellar <43379979+MelissaCuellar@users.noreply.github.com>
2019-03-06 12:36:16 -0500

    Add and Edid Images

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -32,5 +32,16 @@ namespace Shop.Web.Data.Entities
 
         public User User { get; set; }
 
+        public string ImageFullPath
+        {
+            get
+            {
+                if(string.IsNullOrEmpty(this.ImageUrl))
+                {
+                    return null;
+                }
+                return $"https://shopcuellar.azurewebsites.net{this.ImageUrl.Substring(1)}";
+            }
+        }
     }
 }
