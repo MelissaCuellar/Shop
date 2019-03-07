78ea1479b6815948cd7e98bfd6e96cd1a1ea93f8
MelissaCuellar <43379979+MelissaCuellar@users.noreply.github.com>
2019-03-07 10:36:11 -0500

    Show products on Xamarin Form

--- Shop.Web/Controllers/ProductsController.cs
+++ Shop.Web/Controllers/ProductsController.cs
@@ -64,17 +64,20 @@ namespace Shop.Web.Controllers
 
                 if (view.ImageFile != null && view.ImageFile.Length > 0)
                 {
+                    var guid = Guid.NewGuid().ToString();
+                    var file = $"{guid}.jpg";
+
                     path = Path.Combine(
                         Directory.GetCurrentDirectory(), 
                         "wwwroot\\images\\Products", 
-                        view.ImageFile.FileName);
+                        file);
 
                     using (var stream = new FileStream(path, FileMode.Create))
                     {
                         await view.ImageFile.CopyToAsync(stream);
                     }
 
-                    path = $"~/images/Products/{view.ImageFile.FileName}";
+                    path = $"~/images/Products/{file}";
                 }
 
                 var product = this.ToProduct(view,path);
@@ -150,15 +153,18 @@ namespace Shop.Web.Controllers
                     var path = view.ImageUrl;
                     if(view.ImageFile!=null && view.ImageFile.Length>0)
                     {
+                        var guid = Guid.NewGuid().ToString();
+                        var file = $"{guid}.jpg";
+
                         path = Path.Combine(
                             Directory.GetCurrentDirectory(),
                             "wwwroot\\images\\Products",
-                            view.ImageFile.FileName);
+                            file);
                         using (var strem = new FileStream(path, FileMode.Create))
                         {
                             await view.ImageFile.CopyToAsync(strem);
                         }
-                        path = $"~/images/Products/{view.ImageFile.FileName}";
+                        path = $"~/images/Products/{file}";
                     }
                     var product = this.ToProduct(view,path);
                     // TODO: Pending to change to: this.User.Identity.Name
